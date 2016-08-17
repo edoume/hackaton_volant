20ad847fe3c96c9466823a850210a4c600b102d0
Karb0 <carbonel.mathieu@gmail.com>
2016-08-17 17:32:29 +0200

    create a second spot factory

--- spec/factories/spot.rb
+++ spec/factories/spot.rb
@@ -1,7 +1,16 @@
 FactoryGirl.define do
   factory :spot do
     user
-    nom "Sainte Feyre"
-    adresse "7 place du 8 mai"
+
+    factory :fixed_spot do
+      nom "Sainte Feyre"
+      adresse "7 place du 8 mai"
+    end
+
+    factory :invalid_spot do
+      user
+      nom nil
+      adresse nil
+    end
   end
 end
